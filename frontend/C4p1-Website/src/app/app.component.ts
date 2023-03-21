54f3763aeec639cce1f5799c3c2bbf003f16378b
Capi2k <mad.lutz2000rgmail.com>
2023-03-21 21:53:18 +0100

    close sidebar by clicking anything else but sidebr

--- frontend/C4p1-Website/src/app/app.component.ts
+++ frontend/C4p1-Website/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { SidebarService } from './_services/sidebar/sidebar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    '(document:click)': 'onClick($event)'
+  }
 })
 export class AppComponent {
   title = 'C4p1-Website';
+
+  constructor(
+    private eref: ElementRef,
+    private sidebarService: SidebarService
+  ) {
+
+  }
+
+  // Method that enables closing the sidebar with a click anywhere, that isnt the sidebar
+  onClick(event: Event) {
+    if(this.sidebarService.sidebarShow == true) {
+      if (!this.eref.nativeElement.contains(event.target)) {
+        this.sidebarService.sidebarShow = !this.sidebarService.sidebarShow;
+      }
+    }
+  }
+
 }
